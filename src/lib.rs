fa9675ccdce112fe5ec0868927e07aa854b35e33
Christian Sanders <christian.sanders@live.com>
2019-04-12 12:37:49 -0700

    A Bad Stack Take Two

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-pub mod first;
+// pub mod first;
 // pub mod second;
-pub mod third;
-pub mod fourth;
-pub mod fifth;
+// pub mod third;
+// pub mod fourth;
+// pub mod fifth;
+pub mod first_take_two;
